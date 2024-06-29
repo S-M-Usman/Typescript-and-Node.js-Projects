7901fad68491c910c50ccacee58fe90786d6f797
Shaikh Muhammad Usman <shaikhmuhammadusmanofficial@gmail.com>
2024-06-29 13:29:15 +0500

    project 0 calculator

---
+++ project00_calculator/index.ts
@@ -0,0 +1,48 @@
+#! /usr/bin/env node   
+//importing inquirer module and prompting user for inputs and operatio to perfprm
+import inquirer from "inquirer" 
+console.log("Welcome To My CLI Based Calculator");
+const answers = await inquirer.prompt([
+    {
+        name: "num1",
+        type: "number",
+        message: "Enter First Number",
+    },
+    {
+        name: "num2",
+        type: "number",
+        message: "Enter Second Number",
+    },
+    {
+        name: "operation",
+        type: "list",
+        message: "Select Operation to Perform",
+        choices: ["Add", "Subtract", "Multiply", "Divide", "Modulus", "Power",,"Exit"]
+    }
+]);
+// Applying if else conditions so that each type of operation can be performed only if it is selected
+if(answers.operation=="Exit"){
+    console.log("Thank You For Using My CLI Based Calculator");
+    process.exit();
+}
+else if (answers.operation=="Add"){
+    console.log(answers.num1+answers.num2);
+}
+else if (answers.operation=="Subtract"){
+    console.log(answers.num1-answers.num2);
+}
+else if (answers.operation=="Multiply"){
+    console.log(answers.num1*answers.num2);
+}
+else if (answers.operation=="Divide"){
+    console.log(answers.num1/answers.num2);
+}
+else if (answers.operation=="Modulus"){
+    console.log(answers.num1%answers.num2);
+}
+else if (answers.operation=="Power"){
+    console.log(answers.num1**answers.num2);
+}
+else{
+    console.log("Please Select A Valid Operatin!");
+}
---
+++ project00_calculator/readme.md
@@ -0,0 +1,12 @@
+# GOVERNOR SINDH INITIATIVE FOR AI WEB 3.0 AND MATAVERSE
+# Simple Command Line Calculator
+
+Develop a simple command line calculator using [TypeScipt](https://www.typescriptlang.org/), [Node.js](https://nodejs.org/en/) and [Inquirer](https://www.npmjs.com/package/inquirer).
+
+Create a GitHub repository for the project and submit its URL and NPX command in the project submission form. Please note that you will create a NPX command for your project and publish it on npm. After the NPX command is working you will announce it on Panaverse groups on Facebook, Discord, and Twitter.
+Run the following command to experience a CLI calculator:
+
+        npx @smusman/cli-based-calculator
+
+
+
